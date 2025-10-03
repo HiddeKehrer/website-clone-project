112b5388c07b2a215bcddee43468238630b17041
HiddeKehrer <hiddekehrer@gmail.com>
2025-10-03 16:29:44 +0200

    Fix Docusaurus broken links - change baseUrl to root and set onBrokenLinks to warn

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,14 +18,14 @@ const config: Config = {
   url: 'https://pipeguru.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'pipeguru.ai', // Usually your GitHub org/user name.
   projectName: 'documentation', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
