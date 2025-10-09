a49b33130a0134ab4bfd3d5946c41ec8e2ca4c62
Ravdeep Singh <mail@ravdeep.com>
2025-10-09 10:55:43 +0200

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,6 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/legal/privacy',
     '/legal/terms',
     '/legal/imprint',
+    '/docs',
   ].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
